a704313c9e7b7ff214db48599a3e49b2c6ba6fc1
Anant Kapoor <anant.kapoor@outlook.com>
2018-12-07 12:44:01 +0000

    Stanford NER extracted names

--- src/SeminarExtraction.py
+++ src/SeminarExtraction.py
@@ -1,19 +1,23 @@
 import re
 import nltk
 from nltk.tag import StanfordNERTagger
+from nltk import pos_tag
 from nltk.tokenize import word_tokenize
-#nltk.download()
+from nltk.chunk import conlltags2tree
+from nltk.tree import Tree
+
+# nltk.download()
 st = StanfordNERTagger('../english.all.3class.distsim.crf.ser.gz',
                        '../stanford-ner.jar',
                        encoding='utf-8')
 
-x = open("../data/seminars_untagged/304.txt").read()
+x = open("../data/seminars_untagged/301.txt").read()
 header = x[:x.find('Abstract: ')]
 body = x[x.find('Abstract: '):]
 print(header)
 
 
-def timeextract():
+def time_extract():
     timeRegEx = r"([012]?[0-9][:][0-9]{2}?\s?[ap]m)|([012]?[0-9][:][0-9]{2})|([01][0-9]?\s?[ap]m)"
 
     timeMatches = re.finditer(timeRegEx, header, re.MULTILINE | re.IGNORECASE)
@@ -24,7 +28,7 @@ def timeextract():
         break
 
 
-def locationextract():
+def location_extract():
     locationRegEx = r"Place:\s*([^\n]+)"
     locationMatches = re.search(locationRegEx, header, re.MULTILINE | re.IGNORECASE)
 
@@ -39,20 +43,56 @@ def locationextract():
 
 def stanford_tagger(text):
     tokenized_text = word_tokenize(text)
-    classified_text = st.tag(tokenized_text)
-    filtered_text = []
-    for i in classified_text:
-        if i[1] != 'O':
-            filtered_text.append(i)
-
-    # for i in range(len(filtered_text)-1):
-    #     if i[1] == filtered_text[i+1]:
-    #
-    #         i = i+(i+1)
-
-    print(filtered_text)
+    ne_tagged_text = st.tag(tokenized_text)
+    return ne_tagged_text
 
 
-timeextract()
-locationextract()
-stanford_tagger(header)
+def bio_tagger(ne_tagged):
+    bio_tagged = []
+    prev_tag = "O"
+    for token, tag in ne_tagged:
+        if tag == "O":  # O
+            bio_tagged.append((token, tag))
+            prev_tag = tag
+            continue
+        if tag != "O" and prev_tag == "O":  # Begin NE
+            bio_tagged.append((token, "B-" + tag))
+            prev_tag = tag
+        elif prev_tag != "O" and prev_tag == tag:  # Inside NE
+            bio_tagged.append((token, "I-" + tag))
+            prev_tag = tag
+        elif prev_tag != "O" and prev_tag != tag:  # Adjacent NE
+            bio_tagged.append((token, "B-" + tag))
+            prev_tag = tag
+    return bio_tagged
+
+
+def stanford_tree(bio_tagged):
+    tokens, ne_tags = zip(*bio_tagged)
+    pos_tags = [pos for token, pos in pos_tag(tokens)]
+
+    conlltags = [(token, pos, ne) for token, pos, ne in zip(tokens, pos_tags, ne_tags)]
+    ne_tree = conlltags2tree(conlltags)
+    return ne_tree
+
+
+# Parse named entities from tree
+def structure_ne(ne_tree):
+    ne = []
+    for subtree in ne_tree:
+        if type(subtree) == Tree:  # If subtree is a noun chunk, i.e. NE != "O"
+            ne_label = subtree.label()
+            ne_string = " ".join([token for token, pos in subtree.leaves()])
+            ne.append((ne_string, ne_label))
+    return ne
+
+
+def stanford_main():
+    print(structure_ne(stanford_tree(bio_tagger(stanford_tagger(header)))))
+
+
+time_extract()
+location_extract()
+stanford_main()
+
+# stanford_tagger(header)
